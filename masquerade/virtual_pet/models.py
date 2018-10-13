c93f9eb310b51e27776c4f9acbd1ec96d5863ec5
windstormeye <877302410@qq.com>
2018-10-13 22:28:28 +0800

    [v0.0.2] add petid for virtual pet

--- masquerade/virtual_pet/models.py
+++ masquerade/virtual_pet/models.py
@@ -3,12 +3,13 @@ from user.models import MasUser
 
 
 class virtualPet(models.Model):
-    nick_name = models.CharField(max_length=18, blank=False)
+    nick_name = models.CharField(max_length=18, blank=False, unique=True)
     # 0 => male, 1 => female
     gender = models.IntegerField()
     # 0 => 法国斗牛犬, 1 => 威尔士柯基, 2 => 威玛猎犬
     breed = models.IntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
+    pet_id = models.CharField(max_length=10, unique=True)
     last_updated_time = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(MasUser, on_delete=models.CASCADE)
@@ -19,6 +20,7 @@ class virtualPet(models.Model):
             'nick_name': self.nick_name,
             'gender': self.gender,
             'breed': self.breed,
+            'pet_id': self.pet_id,
             'created_time': self.created_time.timestamp(),
         }
         return json
--- masquerade/virtual_pet/views.py
+++ masquerade/virtual_pet/views.py
@@ -1,6 +1,7 @@
 from user.models import MasUser
 from .models import virtualPet
 from common import decorator, utils, masLogger
+import shortuuid
 
 
 @decorator.request_check_args(['breed', 'gender', 'nick_name'])
@@ -11,11 +12,23 @@ def create_virtual_pet(request):
     breed = request.POST.get('breed')
     user_nick_name = request.POST.get('user_nick_name')
 
-    user = MasUser.objects.filter(nick_name=user_nick_name).first()
+    if virtualPet.objects.filter(nick_name=nick_name).exists():
+        masLogger.log(request, 2333, '宠物昵称已存在')
+        return utils.ErrorResponse(2333, '宠物昵称已存在')
 
+    user = MasUser.objects.filter(nick_name=user_nick_name).first()
     if user:
+        shortuuid.set_alphabet('0123456789')
+        pet_id = shortuuid.random(length=8)
+
+        while True:
+            if virtualPet.objects.filter(pet_id=pet_id).exists():
+                pet_id = shortuuid.random(length=8)
+            else:
+                break
+
         virtual_pet = virtualPet(nick_name=nick_name, gender=int(gender),
-                                 breed=int(breed), user=user)
+                                 breed=int(breed), user=user, pet_id=pet_id)
         virtual_pet.save()
 
         json = {
@@ -25,5 +38,5 @@ def create_virtual_pet(request):
         masLogger.log(request, 666)
         return utils.SuccessResponse(json)
     else:
-        masLogger.log(request, 2333, '用户头像已存在')
-        return utils.ErrorResponse(2333, '用户头像已存在')
+        masLogger.log(request, 2333, '用户不存在')
+        return utils.ErrorResponse(2333, '用户不存在')
