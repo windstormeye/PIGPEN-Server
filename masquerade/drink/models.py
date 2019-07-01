143887f5840d0eba88623345e037a702c42455bf
pjhubs <877302410@qq.com>
2019-07-01 22:28:15 +0800

    [v0.0.1] <update> pet drink score API

--- masquerade/drink/models.py
+++ masquerade/drink/models.py
@@ -21,7 +21,6 @@ class Drink(models.Model):
 
     def toJSON(self):
         json = {
-            'id': self.id,
             'waters': int(self.waters),
             'created_time': int(self.created_time.timestamp())
         }
